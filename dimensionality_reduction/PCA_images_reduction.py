aa2420be3b0f4f19415f754317e89d24568a8c24
M. S. Rosito <msrosito@gmail.com>
2022-07-29 14:13:33 -0400

    images dimensionality reduction

--- dimensionality_reduction/PCA_images_reduction.py
+++ dimensionality_reduction/PCA_images_reduction.py
@@ -2,23 +2,19 @@
 # Dimensionality reduction of a set of images using Principal Component Analysis
 #
 # See: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#sklearn.decomposition.PCA.fit
-# See: https://umap-learn.readthedocs.io/en/latest/
 ################################################################################
 
 import sklearn.decomposition as dsl
 import numpy as np
 from PIL import Image, ImageOps
 import os
-import umap
-import sklearn.metrics as skm
-import sklearn.linear_model as lm
 
 ################################################################################
 # Function definitions
 ################################################################################
 
 """
-    principal_components(X):
+    principal_components_skl(X):
 
 Description
     Analysis of principal components of a matrix of features using scikit-learn
@@ -50,7 +46,7 @@ Outputs
     `arr`: flat numpy array (height x width)
     `shape`: shape of the original image in pixels (height, width)
 """   
-   
+    
 def images_to_flat_array(img_filename):
     img = Image.open(img_filename)
     img = ImageOps.grayscale(img)
@@ -58,7 +54,7 @@ def images_to_flat_array(img_filename):
     shape_img = np.shape(arr)
     arr = arr.flatten()
     return arr, shape_img
-
+ 
     
 """
     construct_galaxy_dataset(input_dir, size)
@@ -73,6 +69,7 @@ Outputs
     `X_feat`: numpy matrix of flattened images of the galaxies 
     (n_non-ellipticals + n_ellpiticals, size)
     `y_label`: label of each sample (1 if elliptical, 0 otherwise)
+
 """        
     
 def construct_galaxy_dataset(input_dir, size):
@@ -97,51 +94,9 @@ def construct_galaxy_dataset(input_dir, size):
         imgarr = images_to_flat_array(img_filename)[0]
         X_feat[i] = imgarr
         i += 1
-    return X_feat, y_label    
-    
- 
-"""
-    logistic_regression_fit_and_predict_skl(X_train, y_train, X_test = None, 
-    y_test = None, intercept = True)
-
-Description
-    Logistic regression for classification. Train and test.
-Inputs
-   `X_train`: training features (ntrain x nfeatures matrix)
-   `y_train`: labels of the true class of each observation (ntrain array)
-   `X_test`: test set of features (ntest x nfeatures matrix), optional
-   `y_test`: test set of true labels (ntest x nfeatures matrix), optional
-   `intercept`: a constant should be added to the fitting (default: True)
-Outputs
-    `coef`: coefficients
-    `inter`: intercept
-    `pred_train_class`: predicted class for each training sample (ntrain array)
-    `pred_train_prob`: predicted probabilities for each training sample to
-    belong to each class (ntrain x nclasses)
-    `pred_test_class`: predicted class for each test sample (ntest array)
-    `pred_test_prob`: predicted probabilities for each test sample to
-    belong to each class (ntrain x nclasses)
-    `accuracy`: mean accuracy on the test set  
-"""   
-
-def logistic_regression_fit_and_predict_skl(X_train, y_train, X_test = None, 
-    y_test = None, intercept = True):
-    logreg_fit = lm.LogisticRegression(fit_intercept = intercept).fit(X_train,y_train)
-    coef = logreg_fit.coef_
-    inter = logreg_fit.intercept_
-    pred_train_class = logreg_fit.predict(X_train)
-    pred_train_prob = logreg_fit.predict_proba(X_train)
-    if X_test != None and y_test != None:
-        pred_test_class = logreg_fit.predict(X_test)
-        pred_test_prob = logreg_fit.predict_proba(X_test)
-        accuracy = logreg_fit.score(X_test, y_test)  
-              
-        return coef, inter, pred_train_class, pred_train_prob, pred_test_class, 
-        pred_test_prob, accuracy        
-    else:
-        return coef, inter, pred_train_class, pred_train_prob
-
-                   
+    return X_feat, y_label
+        
+           
 ################################################################################
 # Main
 ################################################################################    
@@ -155,22 +110,14 @@ X, y = construct_galaxy_dataset(input_dir, image_size)
 X = X - X.mean(axis = 0)
 
 # Compute the principal n_comp components
-n_comp = 2
-c, ev, T_pca = principal_components_skl(X, n_comp)
+n_comp = 10
+c, ev, T = principal_components_skl(X, n_comp)
 
-# Compute the UMAP projection with the same number of components
-T_umap = umap.UMAP(n_components = 2, random_state = 0).fit_transform(X)
+# Report the results
+print('Porcentage of variance explained by each component: ')
+for i in range(n_comp):
+    print('PC' + str(i + 1), ': ', np.round(ev[i], 3))
+print('Total explained variance using ', n_comp, ' components: ', np.sum(ev))
 
-# Classification of T_pca using logistic regression
-c_pc, i_pc, pred_class_pc, pred_prob_pc = logistic_regression_fit_and_predict_skl(T_pca, y)
+exit()
 
-# Classification of T_umap using logistic regression
-c_um, i_um, pred_class_um, pred_prob_um = logistic_regression_fit_and_predict_skl(T_umap, y)
-
-# Report results of both classifications
-print('Classification of galaxy images using logistic regression')
-labels = ['Non-ELliptical', 'Elliptical']
-print('Dimensionality reduction of images using PCA ', n_comp, ' components')
-print(skm.classification_report(y, pred_class_pc, target_names = labels))
-print('Dimensionality reduction of images using UMAP ', n_comp, ' components')
-print(skm.classification_report(y, pred_class_um, target_names = labels))
