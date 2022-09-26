33c0614f7bce794347ac52cbd00083311de3fc95
M. S. Rosito <msrosito@gmail.com>
2022-09-26 18:55:45 -0400

    Basic dataframe pandas

---
+++ dataframes-pandas/example1.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+
+input_dir = '../data/'
+output_dir = './resultsPD/'
+
+# Definition of dataframes ####################################################
+
+# Dictionary
+df = pd.DataFrame({'a': [10, 20, 30], 'b': [4, 5, 6], 'c': [7, 8, 9]}, 
+                   index = [1, 2, 3])
+print(df)
+
+mydict = {'a': [10, 20, 30], 'b': [4, 5, 6], 'c': [7, 8, 9]}
+df = pd.DataFrame.from_dict(mydict)
+print(df)
+
+# Matrix - Numpy
+df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index = ['ind1', 'ind2'], 
+                   columns = ['a', 'b', 'c'])
+print(df)
+
+# From csv
+df = pd.read_csv(input_dir + 'data_example_LR.csv')
+print(df)
+# two columns = x, y
+
+# Access to data ##############################################################\
+
+# By index
+print(df.iloc[10: 15]) # rows 10 to 14
+print(df.iloc[:, [1]]) # column 1
+print(df.iloc[[1, 3], [0]]) # rows 1 and 3, column 0
+
+# By label
+print(df.loc[0: 2,  'x']) # first three rows, column 0
+column0 = df['x']
+print((column0)) # column 0
+
+# Single values
+print(df.iat[1, 1]) # row 1, column 1
+print(df.at[4, 'y']) # row 4, column 1
+
+# Add data #####################################################################
+
+# Column
+df['newcol'] = np.zeros(len(df))
+print(df)
+
+# Row
+df.loc[len(df)] = [1, 2, 3]
+print(df)
+newrow = {'x': 500, 'y': 100, 'newcol': 300}
+df = df.append(newrow, ignore_index = True)
+print(df)
+
+# Summarization ################################################################
+print('Information about the data frame')
+print('Number of rows: ', len(df))
+print('Shape: ', df.shape)
+print('Number of distinct values of newcol: ', df['newcol'].nunique())
+print('Number of repetitions of each value in newcol: ', df['newcol'].value_counts())
+print('Sumamary', df.describe())
+
+# Save dataframe
+df.to_csv(output_dir + 'df.csv')
+df.to_csv(output_dir + 'df-noindex.csv', index = False) # remove indexes
